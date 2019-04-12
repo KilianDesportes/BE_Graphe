0fb8ca7bc9f56e73c15bde28cf6edffdb99262e5
Kilian <kilian.desportes@gmail.com>
2019-04-12 23:50:04 +0200

    couts

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -19,7 +19,6 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
 		Node origin_node = data.getOrigin();
 		Node dest_node = data.getDestination();
-		Node current_node = origin_node;
 
 		List<Node> al_Nodes = data.getGraph().getNodes();
 		ArrayList<Label> al_Labels = Label.InitGraphNode(al_Nodes);
@@ -33,7 +32,7 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
 		bh_tas.insert(origin_node);
 
-		int nb_nodes_unmarqued = al_Nodes.size() - 1;
+		int nb_nodes_unmarqued = al_Nodes.size();
 
 		int changement_pere = 0;
 
@@ -74,6 +73,13 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 				System.out.println("Label marked " + lab_node.isMarque());
 
 				if (lab_node.isMarque() == false) {
+					
+					/*PRENDRE EN COMPTE LES AJOUTS DE COUTS AU FUR ET A MESURE DES 
+					 * ETAPES DE PARCOURS. PAS SEULEMENT COMPARAISON AU COUT DE L'ARC 
+					 * ACTUEL. GL 
+					 */
+					
+					
 					System.out.println("Label non marqué");
 					System.out.println("Cout actuel label " + lab_node.getCoût());
 					System.out.println("Cout de l'arc " + arc_node.getLength());
@@ -100,15 +106,15 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 		System.out.println("Dest " + dest_node);
 		System.out.println("Changement père " + changement_pere);
 		for (int y = 0; y < al_Labels.size(); y++) {
-			if (al_Labels.get(y).getPere() != null) {
+//			if (al_Labels.get(y).getPere() != null) {
 				System.out.println("___");
 				System.out.println("" + y + "/" + al_Labels.size());
 				System.out.println("Cost " + al_Labels.get(y).getCoût());
 				System.out.println("Current node " + al_Labels.get(y).getSommet_courant());
 				System.out.println("Père " + al_Labels.get(y).getPere());
-				System.out.println(al_Labels.get(y).getPere().getOrigin());
-				System.out.println(al_Labels.get(y).getPere().getDestination());
-			}
+//				System.out.println(al_Labels.get(y).getPere().getOrigin());
+//				System.out.println(al_Labels.get(y).getPere().getDestination());
+//			}
 
 		}
 
